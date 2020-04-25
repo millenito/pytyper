eb46855e0ca32946e2eef2d7ff1072b64d273eed
millenito <wownito31@gmail.com>
2020-04-26 00:15:29 +0700

    make falling works random from random_words library and implement text box

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 import pygame
 import sys
 import random
+from random_words import RandomWords
 from word import Word
+import pygame_textinput
 
 
 class Game:
@@ -12,42 +14,74 @@ class Game:
         self.game_font = pygame.font.Font("freesansbold.ttf", 32)
         self.bg_color = pygame.Color('black')
         self.white = (255, 255, 255)
+        self.red = (255, 0, 0)
         self.green = (0, 255, 0)
         self.blue = (0, 0, 255)
-        self.words_list = ['oke', 'deh',
-                           'katapanjang', 'kata', 'word', 'zyrex']
-        self.words = []
+        self.score = 0
+        self.falling_words = []
+        self.right = []
 
     def gen_words(self):
+        rw = RandomWords()
         for i in range(50):
-            rand_words = random.choice(self.words_list)
-            self.words.append(Word(rand_words, self.screen,
-                                   self.game_font, self.white, self.bg_color))
+            rand_words = rw.random_word()
+            while len(rand_words) > 10:
+                rand_words = rw.random_word()
 
-            self.words[i].draw()
+            self.falling_words.append(Word(rand_words, self.screen,
+                                           self.game_font, self.white, self.bg_color))
+            self.falling_words[i].draw()
+
+    def draw(self):
+        # Blue Borders
+        pygame.draw.rect(self.screen, self.red, (0, self.screen_h - 170,
+                                                 self.screen_w + 4, (self.screen_h - 300) + 4), 0)
+        # Black box inside blue border
+        pygame.draw.rect(self.screen, self.blue, (10, self.screen_h - 160,
+                                                  self.screen_w - 20, self.screen_h - 300), 0)
+
+        game_score = self.game_font.render(f"{self.score}", True, self.white)
+        if self.score > 0:
+            self.screen.blit(
+                game_score, (self.screen_w - 95, self.screen_h - 140))
 
     def run(self):
         clock = pygame.time.Clock()
+        textbox = pygame_textinput.TextInput(antialias=True,
+                                             text_color=self.white, cursor_color=self.white)
+        textinput = ""
 
         while True:
-            for event in pygame.event.get():
+            events = pygame.event.get()
+            for event in events:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
             self.screen.fill(self.bg_color)
-            self.gen_words()
 
-            # Blue Borders
-            pygame.draw.rect(self.screen, self.blue, (0, self.screen_h - 170,
-                                                      self.screen_w + 4, (self.screen_h - 300) + 4), 0)
-            # Black box inside blue border
-            pygame.draw.rect(self.screen, self.bg_color, (10, self.screen_h - 160,
-                                                          self.screen_w - 20, self.screen_h - 300), 0)
-            for w in self.words:
+            if textbox.update(events):
+                textinput = textbox.get_text()
+
+                for w in self.falling_words:
+                    if textinput == w.word and w.y > 0 and w.y < self.screen_h - 165:
+                        self.score += 100
+                        w.delete()
+                        textbox.clear_text()
+                        self.right.append(w.word)
+
+            self.gen_words()
+            self.draw()
+
+            # Draw textbox if there's no correct typed words or no text inputed
+            if not textinput or self.right is not None:
+                self.screen.blit(textbox.get_surface(),
+                                 (25, self.screen_h - 145))
+
+            for w in self.falling_words:
                 w.y += 3
-                if w.y >= self.screen_h - 165:
-                    print('kena bawah')
+                if w.hit_bottom():
+                    print(w.word)
 
             pygame.display.flip()  # Outputs to display
             clock.tick(60)  # Game tick 60 FPS
--- word.py
+++ word.py
@@ -12,13 +12,15 @@ class Word:
         self.screen_w, self.screen_h = pygame.display.get_surface().get_size()
         self.x = random.randint(0, self.screen_h - 300)
         self.y = random.randint(-1000, 0) * 3
+        self.typed = False
 
     def draw(self):
         render = self.game_font.render(self.word, True, self.color)
-
         self.screen.blit(render, (self.x, self.y))
 
     def delete(self):
-        render = self.game_font.render(self.word, True, self.bg_color)
+        self.typed = True
+        self.color = self.bg_color
 
-        self.screen.blit(render, (self.x, self.y))
+    def hit_bottom(self):
+        return True if not self.typed and self.y >= self.screen_h - 165 else False
