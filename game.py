0814e4b6160242e69f664cc1b24304503112c163
millenito <wownito31@gmail.com>
2020-04-27 17:39:59 +0700

    fix invisible words hitting bottom and add some text

--- game.py
+++ game.py
@@ -12,6 +12,7 @@ class Game:
         self.mode = mode
         self.screen_w, self.screen_h = pygame.display.get_surface().get_size()
         self.game_font = pygame.font.Font("freesansbold.ttf", 32)
+        self.text_font = pygame.font.Font("freesansbold.ttf", 24)
         self.bg_color = pygame.Color('black')
         self.white = (255, 255, 255)
         self.red = (255, 0, 0)
@@ -34,30 +35,50 @@ class Game:
             self.falling_words[i].draw()
 
     def draw(self):
-        # Blue Borders
+        # Red Borders
         pygame.draw.rect(self.screen, self.red, (0, self.screen_h - 170,
                                                  self.screen_w + 4, (self.screen_h - 300) + 4), 0)
-        # Black box inside blue border
+        # Blue box inside red border
         pygame.draw.rect(self.screen, self.blue, (10, self.screen_h - 160,
                                                   self.screen_w - 20, self.screen_h - 300), 0)
 
-        game_score = self.game_font.render(str(self.score), True, self.white)
-        if self.score > 0:
-            self.screen.blit(
-                game_score, (self.screen_w - 95, self.screen_h - 140))
+        # Game Score
+        game_score = self.text_font.render(
+            f"Score: {self.score}", True, self.white)
+        self.screen.blit(game_score,
+                         (self.screen_w - 155, self.screen_h - 140))
+
+        # Correct Words
+        correct_words = self.text_font.render(
+            f"Words: {len(self.right)}", True, self.white)
+        self.screen.blit(correct_words,
+                         (self.screen_w - 155, self.screen_h - 60))
 
     def speedup(self, start_timer):
         current_time = pygame.time.get_ticks()
         seconds_passed = current_time - start_timer
 
         if seconds_passed >= 15000:
-            self.fall_speed = 2
+            self.fall_speed = 1.5
+            speedup_text = self.game_font.render("Speedup!", True, self.red)
+            self.screen.blit(speedup_text,
+                             (self.screen_w - 500, self.screen_h - 110))
         elif seconds_passed >= 30000:
-            self.fall_speed = 3
+            self.fall_speed = 2
+            speedup_text = self.game_font.render("Speedup x2!", True, self.red)
+            self.screen.blit(speedup_text,
+                             (self.screen_w - 500, self.screen_h - 110))
         elif seconds_passed >= 90000:
-            self.fall_speed = 4
+            self.fall_speed = 2.5
+            speedup_text = self.game_font.render("Speedup x2.5!", True,
+                                                 self.red)
+            self.screen.blit(speedup_text,
+                             (self.screen_w - 500, self.screen_h - 110))
         elif seconds_passed >= 150000:
-            self.fall_speed = 5
+            self.fall_speed = 3
+            speedup_text = self.game_font.render("Speedup x3!", True, self.red)
+            self.screen.blit(speedup_text,
+                             (self.screen_w - 500, self.screen_h - 110))
 
     def run(self):
         clock = pygame.time.Clock()
@@ -73,20 +94,20 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
-
             if textbox.update(events):
                 textinput = textbox.get_text()
 
                 for w in self.falling_words:
                     if textinput == w.word and w.y > 0 and w.y < self.screen_h - 165:
-                        self.score += 100
+                        self.right.append(w.word)
+                        self.right = list(set(self.right))  # Make unique
+                        self.score = len(self.right) * 100
                         w.delete()
                         textbox.clear_text()
-                        self.right.append(w.word)
 
-            self.gen_words()
+            self.screen.fill(self.bg_color)
             self.draw()
+            self.gen_words()
             self.speedup(start_timer)
 
             # Draw textbox if there's no correct typed words or no text inputed
@@ -95,10 +116,16 @@ class Game:
                                  (25, self.screen_h - 145))
 
             for w in self.falling_words:
-                w.y += self.fall_speed
-                if w.hit_bottom():
-                    print(w.word)
-                    break
+                if not w.typed and w.drawed:
+                    w.y += self.fall_speed
+                    if w.hit_bottom():
+                        print(
+                            f"{w.word} x: {w.x} y: {w.y} color: {w.color} typed: {w.typed}")
+                        w.color = self.green
+                        #  w.y = self.screen_h / 2
+                        #  w.x = self.screen_w / 2
+                        #  print(f"{w.word} x: {w.x} y: {w.y}")
+                        #  break
 
             pygame.display.flip()  # Outputs to display
             clock.tick(60)  # Game tick 60 FPS
--- word.py
+++ word.py
@@ -10,17 +10,22 @@ class Word:
         self.color = color
         self.bg_color = bg_color
         self.screen_w, self.screen_h = pygame.display.get_surface().get_size()
-        self.x = random.randint(0, self.screen_h - 300)
+        self.x = random.randint(0, self.screen_w - 300)
         self.y = random.randint(-1000, 0) * 3
         self.typed = False
+        self.drawed = False
 
     def draw(self):
         render = self.game_font.render(self.word, True, self.color)
         self.screen.blit(render, (self.x, self.y))
+        self.drawed = True
 
+    # Set typed to true and move offscreen
     def delete(self):
         self.typed = True
+        self.x = -300
         self.color = self.bg_color
 
     def hit_bottom(self):
-        return True if not self.typed and self.y >= self.screen_h - 165 else False
+        #  return True if not self.typed and self.x <= self.screen_w - 300 and self.y > self.screen_h - 170 else False
+        return True if not self.typed and self.drawed and self.y > self.screen_h - 170 else False
